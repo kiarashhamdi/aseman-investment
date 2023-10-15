33edaeec6cfd0663a027a901183f1e479375ac6f
kiarash hamdi <89359870+kiarashhamdi@users.noreply.github.com>
2023-10-15 12:11:36 +0330

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    440: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 4,
+    },
+  },
+});
